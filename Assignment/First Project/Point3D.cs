7b860afe5c511ca715a7b682f63fbaf446f64894
Mazen2810 <mazenmuhammed52@gmail.com>
2025-01-21 23:01:51 +0200

    First Project

---
+++ Assignment/First Project/Point3D.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.First_Project
+{
+    class Point3D : IComparable<Point3D> , ICloneable
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+
+        public Point3D()
+        {
+
+        }
+        public Point3D(int X, int Y, int Z)
+        {
+            this.X = X;
+            this.Y = Y;
+            this.Z = Z;
+        }
+
+
+        #region Overloading Methods [Relational Operator]
+        public static bool operator ==(Point3D left, Point3D right)
+        {
+            if (left.X == right.X && left.Y == right.Y && left.Z == right.Z)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator !=(Point3D left, Point3D right)
+        {
+            if (left.X == right.X && left.Y == right.Y && left.Z == right.Z)
+                return false;
+            else
+                return true;
+        } 
+        #endregion
+
+        public override string ToString()
+        {
+            return $"Point Coordinates: ({X}, {Y}, {Z})";
+        }
+
+       
+
+        public int CompareTo(Point3D? other)
+        {
+     
+
+            int CompareX = this.X.CompareTo(other?.X);
+            if(CompareX !=0) 
+                return CompareX;
+
+            return this.Y.CompareTo(other?.Y);
+        }
+
+        public object Clone()
+        {
+            return new Point3D(X,Y,Z)
+            {
+                X = this.X,
+                Y = this.Y,
+                Z = this.Z
+            };
+        }
+    }
+}
---
+++ Assignment/Program.cs
@@ -0,0 +1,65 @@
+﻿using Assignment.First_Project;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region First Project
+            // int X1 = GetValidInteger("Enter X1: ");
+            // int Y1 = GetValidInteger("Enter Y1: ");
+            // int Z1 = GetValidInteger("Enter Z1: ");
+            // int X2 = GetValidInteger("Enter X2: ");
+            // int Y2 = GetValidInteger("Enter Y2: ");
+            // int Z2 = GetValidInteger("Enter Z2: ");
+
+            // Point3D P1 = new Point3D(X1, Y1, Z1);
+            // Point3D P2 = new Point3D(X2, Y2, Z2);
+
+            //if(P1 == P2)
+            // {
+            //     Console.WriteLine("Yes");
+            // }
+            //else
+            //     Console.WriteLine("No");
+
+
+            //Point3D[] Point = new Point3D[3];
+            //Point[0] = new Point3D(1, 2, 3);
+            //Point[1] = new Point3D(10, 20, 30);
+            //Point[2] = new Point3D(5, 22, 32);
+
+            //Array.Sort(Point);
+            //foreach(Point3D point in Point)
+            //{
+            //    Console.WriteLine(point);
+            //}
+
+            //Point3D P1 = new Point3D(10,20,30);
+
+            //Point3D P2 = new Point3D();
+            //P2 = (Point3D) P1.Clone();
+
+            //Console.WriteLine(P2); 
+            #endregion
+        }
+        public static int GetValidInteger(string Message)
+        {
+            int result;
+            bool isValid;
+            do
+            {
+                Console.Write(Message);
+                isValid = int.TryParse(Console.ReadLine(), out result);
+                if (!isValid)
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid integer.");
+                }
+            } while (!isValid);
+
+            return result;
+        }
+    }
+    
+}
